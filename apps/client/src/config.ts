0db6d8413e4be36eb17aed0544fe60e63757dd5f
Jakub Jóźwiak <kubencki@gmail.com>
2023-11-14 17:33:42 +0100

    chore(client): test vercel envs

--- apps/client/src/config.ts
+++ apps/client/src/config.ts
@@ -3,11 +3,13 @@ import { z } from 'zod';
 const configSchema = z.object({
   API_URL: z.string().default('http://localhost:3000'),
   NEXT_PUBLIC_VERCEL_URL: z.string().default('localhost:3000'),
+  NEXT_PUBLIC_HOST_NAME: z.string().default('localhost:3000'),
   NODE_ENV: z.enum(['development', 'production', 'test']).default('development'),
 });
 
 export const config = configSchema.parse({
   API_URL: process.env.NEXT_PUBLIC_API_URL,
   NEXT_PUBLIC_VERCEL_URL: process.env.NEXT_PUBLIC_VERCEL_URL, //The domain name of the generated deployment URL. Example: *.vercel.app. The value does not include the protocol scheme https://
+  NEXT_PUBLIC_HOST_NAME: process.env.NEXT_PUBLIC_HOST_NAME,
   NODE_ENV: process.env.NODE_ENV,
 });
--- apps/client/src/pages/_app.tsx
+++ apps/client/src/pages/_app.tsx
@@ -22,6 +22,7 @@ type CustomAppProps = AppProps & {
 
 function CustomApp({ Component, pageProps, router }: CustomAppProps) {
   const isDev = config.NODE_ENV === 'development';
+  console.log('L:25 | config: ', config);
 
   return (
     <Providers navigate={router.push} dehydratedState={pageProps.dehydratedState}>
