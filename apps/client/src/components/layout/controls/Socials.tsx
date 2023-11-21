cbc145bfe258b9ec2f746312b02d8b9f1696e180
Jakub Jóźwiak <kubencki@gmail.com>
2023-11-21 11:04:05 +0100

    feat(client): add github star button

--- apps/client/src/components/layout/controls/Socials.tsx
+++ apps/client/src/components/layout/controls/Socials.tsx
@@ -1,11 +1,24 @@
 import { GitHubLogo, LinkedInLogo } from '@/client/assets/socials';
-import { Button, Link, Tooltip } from '@nextui-org/react';
+import { Button, Link } from '@nextui-org/react';
 import { CodeBracketIcon } from '@heroicons/react/24/outline';
+import GitHubButton from 'react-github-btn';
+import { useTheme } from 'next-themes';
 
-export const Socials = () => (
-  <div className="pt-safe-offset-2 pl-safe-offset-4 fixed top-0 left-0 z-50">
-    <div className="flex items-center gap-1 sm:flex-col">
-      <Tooltip content="Check out Jakub Jóźwiak GitHub profile">
+export const Socials = () => {
+  const { resolvedTheme } = useTheme();
+
+  return (
+    <div className="pt-safe-offset-2 pl-safe-offset-4 fixed top-0 left-0 z-50">
+      <div className="flex items-start gap-1 sm:flex-col">
+        <GitHubButton
+          href="https://github.com/Jozwiaczek/pwa-tech-talk"
+          data-color-scheme={resolvedTheme}
+          data-size="large"
+          data-show-count="true"
+          aria-label="Star Jozwiaczek/pwa-tech-talk on GitHub"
+        >
+          Star
+        </GitHubButton>
         <Button
           isIconOnly
           variant="light"
@@ -16,8 +29,6 @@ export const Socials = () => (
         >
           <GitHubLogo className="size-6" />
         </Button>
-      </Tooltip>
-      <Tooltip content="Check out PWA Tech Talk code repository">
         <Button
           isIconOnly
           variant="light"
@@ -28,8 +39,6 @@ export const Socials = () => (
         >
           <CodeBracketIcon className="size-8" />
         </Button>
-      </Tooltip>
-      <Tooltip content="Check out Jakub Jóźwiak LinkedIn profile">
         <Button
           isIconOnly
           variant="light"
@@ -40,7 +49,7 @@ export const Socials = () => (
         >
           <LinkedInLogo className="size-6" />
         </Button>
-      </Tooltip>
+      </div>
     </div>
-  </div>
-);
+  );
+};
