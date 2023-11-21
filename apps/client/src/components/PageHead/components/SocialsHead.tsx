8c4dcee94e3744bb09742e083e983892ca5aefdd
Jakub Jóźwiak <kubencki@gmail.com>
2023-11-21 22:35:15 +0100

    fix(client): adjust screenshots and social-card

--- apps/client/src/components/PageHead/components/SocialsHead.tsx
+++ apps/client/src/components/PageHead/components/SocialsHead.tsx
@@ -18,13 +18,13 @@ export const SocialsHead = ({
     <meta name="twitter:description" content={pageDescription} />
     <meta name="twitter:creator" content="@jozwiaczek" />
     <meta name="twitter:creator:id" content="395940297" />
-    <meta name="twitter:image" content={`${siteUrl}/icons/social-card.png`} />
+    <meta name="twitter:image" content={`${siteUrl}/icons/social-card.jpg`} />
 
     <meta property="og:type" content="website" />
     <meta property="og:title" content={pageTitle} />
     <meta property="og:description" content={pageDescription} />
     <meta property="og:site_name" content={appTitle} />
     <meta property="og:url" content={siteUrl} />
-    <meta property="og:image" content={`${siteUrl}/icons/social-card.png`} />
+    <meta property="og:image" content={`${siteUrl}/icons/social-card.jpg`} />
   </>
 );
