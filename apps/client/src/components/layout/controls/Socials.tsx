2ee6d77dc2968088acb1c61e431e81db1a1043bf
Jakub Jóźwiak <kubencki@gmail.com>
2023-11-21 10:54:16 +0100

    feat(client): add github repo link

--- apps/client/src/components/layout/controls/Socials.tsx
+++ apps/client/src/components/layout/controls/Socials.tsx
@@ -1,21 +1,46 @@
 import { GitHubLogo, LinkedInLogo } from '@/client/assets/socials';
-import { Button, Link } from '@nextui-org/react';
+import { Button, Link, Tooltip } from '@nextui-org/react';
+import { CodeBracketIcon } from '@heroicons/react/24/outline';
 
 export const Socials = () => (
   <div className="pt-safe-offset-2 pl-safe-offset-4 fixed top-0 left-0 z-50">
     <div className="flex items-center gap-1 sm:flex-col">
-      <Button isIconOnly variant="light" as={Link} isExternal href="https://github.com/Jozwiaczek">
-        <GitHubLogo className="size-6" />
-      </Button>
-      <Button
-        isIconOnly
-        variant="light"
-        as={Link}
-        isExternal
-        href="https://www.linkedin.com/in/jozwiakjakub/"
-      >
-        <LinkedInLogo className="size-6" />
-      </Button>
+      <Tooltip content="Check out Jakub Jóźwiak GitHub profile">
+        <Button
+          isIconOnly
+          variant="light"
+          as={Link}
+          isExternal
+          aria-label="Check out Jakub Jóźwiak GitHub profile"
+          href="https://github.com/Jozwiaczek"
+        >
+          <GitHubLogo className="size-6" />
+        </Button>
+      </Tooltip>
+      <Tooltip content="Check out PWA Tech Talk code repository">
+        <Button
+          isIconOnly
+          variant="light"
+          as={Link}
+          isExternal
+          aria-label="Check out PWA Tech Talk code repository"
+          href="https://github.com/Jozwiaczek/pwa-tech-talk"
+        >
+          <CodeBracketIcon className="size-8" />
+        </Button>
+      </Tooltip>
+      <Tooltip content="Check out Jakub Jóźwiak LinkedIn profile">
+        <Button
+          isIconOnly
+          variant="light"
+          as={Link}
+          isExternal
+          aria-label="Check out Jakub Jóźwiak LinkedIn profile"
+          href="https://www.linkedin.com/in/jozwiakjakub/"
+        >
+          <LinkedInLogo className="size-6" />
+        </Button>
+      </Tooltip>
     </div>
   </div>
 );
